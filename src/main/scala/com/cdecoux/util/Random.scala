56e98271a122067e1306b34049ec0e8689f58683
Caleb DeCoux <decouxc82@gmail.com>
2020-01-27 21:49:16 -0600

    added doc to randomizing functions

--- src/main/scala/com/cdecoux/util/Random.scala
+++ src/main/scala/com/cdecoux/util/Random.scala
@@ -7,7 +7,15 @@ object Random {
 
     def selectFromDistribution(probabilities: Array[Double]): Int = selectFromDistribution(probabilities.map(x => x.toFloat))
 
-
+    /**
+     * This function takes in an iterable object that pairs some object with a float value, which defines a custom random distribution for each object key
+     * i.e Map(("a" -> 0.4f), ("b" -> 0.3f), ("c" -> 0.3f)) defines 3 Strings "a", "b", and "c" are selected with 40%, 30%, and 30% chance respectively
+     *
+     * @param m Iterable object (like a Map) which pairs objects with a custom probability
+     * @tparam T Object type to be paired with the probabilities, and returned as selection
+     * @throws RandomDistributionError Returned from error in selecting from distribution. Likely cause is if the probabilities don't add to 100%. Float values need to add to 1
+     * @return The randomly selected object
+     */
     @throws[RandomDistributionError]
     def selectFromDistribution[T](m: Map[T, Float]): T = {
 
@@ -42,7 +50,17 @@ object Random {
     }
 
 
-    // TODO: Evaluate style , compare with mapped version above
+    /**
+     * This function takes in an array of floats that defines a custom random distribution.
+     * i.e [0.1f, 0.5f, 0.4f] define 3 indexes, 0, 1, and 2 which are selected with 10%, 50%, and 40% chance respectively
+     *
+     * This is usually paired with a parallel array. The return index maps with the other returned array index. This function has no context of that array.
+     * See also: selectFromDistribution[T](m: Map[T, Float]): T
+     *
+     * @param probabilities An array of floats that define different distribution probabilities.
+     * @throws RandomDistributionError Error when calculating random distribution. Most likely cause is float values do not add up to 1
+     * @return The index that was randomly selected
+     */
     @throws[RandomDistributionError]
     def selectFromDistribution(probabilities: Array[Float]): Int = {
 
